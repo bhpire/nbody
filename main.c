d3dd29af93cef72b6dc57843122d2693e6df88eb
Chi-kwan Chan <chanc@email.arizona.edu>
2018-12-05 01:37:39 -0700

    Feature: profile the algorithm
    We use gettimeofday() to time the algorithm.  Its resolution is
    unspecified but at least as good the ANSI C clock() function.  On my
    laptop I get ~ 250 ns/step.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include "nbody.h"
 #include <stdio.h>
 #include <signal.h>
+#include <sys/time.h>
 
 static volatile int running = 1;
 
@@ -16,12 +17,21 @@ int main(int argc, char *argv[])
   const int    ns = 100; /* number of sub-steps between outputs */
   int i; /* sub-step index */
 
+  struct timeval t0, t1;
+  double wtime; /* "wall" clock time between each outputs */
+
   signal(SIGINT, handler);
 
   while(running) {
     dump();
+
+    gettimeofday(&t0, NULL);
     for(i = 0; i < ns; ++i)
       step(dt);
+    gettimeofday(&t1, NULL);
+
+    wtime = (t1.tv_sec - t0.tv_sec) + 1.0e-6 * (t1.tv_usec - t0.tv_usec);
+    fprintf(stderr, "%g ns/step\n", 1.0e9 * wtime / ns);
   }
 
   return 0;
