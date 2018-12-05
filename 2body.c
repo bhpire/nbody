9a1591b038ba98796f6a675ebcc0fe51ddc576b6
Chi-kwan Chan <chanc@email.arizona.edu>
2018-12-05 03:49:20 -0700

    Architecture: define kick() and drift() to clarify the code

--- 2body.c
+++ 2body.c
@@ -21,59 +21,41 @@ int dump(FILE *file)
                  r1.x, r1.y, r1.z, r2.x, r2.y, r2.z) > 0;
 }
 
+static inline void kick(double dt)
+{
+  const vector dr = {r1.x - r2.x, r1.y - r2.y, r1.z - r2.z};
+  const double rr = dr.x * dr.x + dr.y * dr.y + dr.z * dr.z;
+  const double dt_rrr = dt / (rr * sqrt(rr));
+
+  v1.x -= dr.x * dt_rrr;
+  v1.y -= dr.y * dt_rrr;
+  v1.z -= dr.z * dt_rrr;
+
+  v2.x += dr.x * dt_rrr;
+  v2.y += dr.y * dt_rrr;
+  v2.z += dr.z * dt_rrr;
+}
+
+static inline void drift(double dt)
+{
+  r1.x += v1.x * dt;
+  r1.y += v1.y * dt;
+  r1.z += v1.z * dt;
+
+  r2.x += v2.x * dt;
+  r2.y += v2.y * dt;
+  r2.z += v2.z * dt;
+}
+
 void evol(int n, double dt)
 {
-  vector dr;
-  double rr, kdt_rrr;
-
   double kdt = dt / 2; /* the first kick is a half step */
   int i;
-
   for(i = 0; i < n; ++i) {
-    /* Get force and kick */
-    dr.x = r1.x - r2.x;
-    dr.y = r1.y - r2.y;
-    dr.z = r1.z - r2.z;
-
-    rr = dr.x * dr.x + dr.y * dr.y + dr.z * dr.z;
-    kdt_rrr = kdt / (rr * sqrt(rr));
-
-    v1.x -= dr.x * kdt_rrr;
-    v1.y -= dr.y * kdt_rrr;
-    v1.z -= dr.z * kdt_rrr;
-
-    v2.x += dr.x * kdt_rrr;
-    v2.y += dr.y * kdt_rrr;
-    v2.z += dr.z * kdt_rrr;
-
-    /* Drift */
-    r1.x += v1.x * dt;
-    r1.y += v1.y * dt;
-    r1.z += v1.z * dt;
-
-    r2.x += v2.x * dt;
-    r2.y += v2.y * dt;
-    r2.z += v2.z * dt;
-
-    /* all other kicks are full steps */
-    kdt = dt;
+    kick(kdt);
+    drift(dt);
+    kdt = dt; /* all other kicks are full steps */
   }
   /* Last half-step correction */
-  kdt = dt / 2;
-
-  /* Get force and kick */
-  dr.x = r1.x - r2.x;
-  dr.y = r1.y - r2.y;
-  dr.z = r1.z - r2.z;
-
-  rr = dr.x * dr.x + dr.y * dr.y + dr.z * dr.z;
-  kdt_rrr = kdt / (rr * sqrt(rr));
-
-  v1.x += dr.x * kdt_rrr;
-  v1.y += dr.y * kdt_rrr;
-  v1.z += dr.z * kdt_rrr;
-
-  v2.x -= dr.x * kdt_rrr;
-  v2.y -= dr.y * kdt_rrr;
-  v2.z -= dr.z * kdt_rrr;
+  kick(dt / 2);
 }
