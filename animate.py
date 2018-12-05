9b0fab28fdc585abb1ff47d2e6f54c0d8baebf99
Chi-kwan Chan <chanc@email.arizona.edu>
2018-12-05 04:55:39 -0700

    Feature: `animate.py` can plot multiple lines

--- animate.py
+++ animate.py
@@ -13,17 +13,21 @@ if len(sys.argv) == 1:
 input = sys.argv[1]
 name, _ = os.path.splitext(input)
 r = np.genfromtxt(name+'.out' if _ == '' else input)
+n = r.shape[1] // 3
 
 fig, ax = plt.subplots()
-plot,   = ax.plot(r[:10,0], r[:10,1])
+lines = []
+for i in range(n):
+    l, = ax.plot(r[:10,i*3+0], r[:10,i*3+1])
+    lines.append(l)
 ax.set_aspect('equal')
 ax.set_xlim([-3,3])
 ax.set_ylim([-2,2])
 
-def update(i):
-    plot.set_xdata(r[i:i+10,0])
-    plot.set_ydata(r[i:i+10,1])
-    return plot
+def update(j):
+    for i, l in enumerate(lines):
+        l.set_data(r[j:j+10,i*3+0], r[j:j+10,i*3+1])
+    return lines
 
 ani = anm.FuncAnimation(fig, update, interval=40)
 
